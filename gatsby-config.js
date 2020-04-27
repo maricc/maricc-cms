8e50bf6b96931173af632c845db64acdffad5c18
Javier Cabrera <cabrera.javier@gmail.com>
2020-04-27 20:14:39 +0200

    added netlify cms and blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,17 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-  },
+  },  
   plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: 'markdown-pages',
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -30,6 +39,5 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,    
-    `gatsby-plugin-netlify-cms`,
   ],
 }
